1fdaa54bea771e4652f295d2bd9d767bbc624fa7
DYDKA4 <hijito@yandex.ru>
2022-10-12 21:18:07 +0300

    1.1 Done

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # This is a sample Python script.
+import torch.nn
+
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
@@ -11,6 +13,7 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    torch.nn.CrossEntropyLoss
+    torch.nn.ModuleList
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
